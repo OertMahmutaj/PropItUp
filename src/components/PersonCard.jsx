afbadcb07347cee5a027186fde32f83a277756f5
Oert Mahmutaj <102991129+Ramasiken@users.noreply.github.com>
2023-08-16 20:55:01 +0200

    first commit

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,12 @@
+const PersonCard = (props) => {
+
+    return (
+
+        <>
+            <h2>{props.lastName}, {props.firstName}</h2>
+            <p>Age: {props.age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+        </>
+    )
+}
+export default PersonCard;
